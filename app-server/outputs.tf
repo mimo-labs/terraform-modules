47db88368e74ba75b0f8baf9999472f4bfe8df8e
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-08-10 01:00:23 -0300

    removes deprecated server modules

--- app-server/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "droplet_ipv4" {
-  value = digitalocean_droplet.this.ipv4_address
-}
-
-output "droplet_fqdn" {
-  value = length(digitalocean_record.droplet_external) > 0 ? digitalocean_record.droplet_external[0].fqdn : ""
-}
